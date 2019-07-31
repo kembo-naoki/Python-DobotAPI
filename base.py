7ef7c5a7955ab084e310b4d94e39ddbe727dc984
kembo <n.kembo@gmail.com>
2019-07-31 13:49:01 +0900

    base.py, coordinate.py における型アノテーションの導入及びコメントの再整理

--- base.py
+++ base.py
@@ -2,11 +2,11 @@ from os  import path
 from abc import (ABCMeta, abstractmethod)
 from ctypes import cdll 
 
-_CUR_DIR = path.dirname( path.abspath( __file__ ) )
+_CUR_DIR = path.dirname(path.abspath( __file__ ))
 API = cdll.LoadLibrary(_CUR_DIR + "/libDobotDll.so.1.0.0")
 
 class CommandModule(metaclass=ABCMeta):
-    """ Dobot の機能毎のクラス """
+    """ Dobot の各機能を表す抽象クラス。 """
     @abstractmethod
     def __init__(self, dobot):
         self.dobot = dobot
@@ -14,7 +14,7 @@ class CommandModule(metaclass=ABCMeta):
         self.requirements = ()
     
     def check_settings(self, *opts):
-        """ このコマンドが送信可能か否か """ 
+        """ self.check_list を利用してコマンドを実行可能かを検査します。 """ 
         req_list = self.requirements + opts
         message = None
         for req in req_list:
--- coordinate.py
+++ coordinate.py
@@ -1,28 +1,13 @@
-from abc import (ABCMeta, abstractmethod)
-from collections.abc import Mapping
+import ctypes
 
-# methods
-def convert_coord(val, relative):
-    if not isinstance(val, (Coordinate, dict)):
-        raise TypeError("It must be dict or dobot.coordinate.Coordinate.")
-    if relative:
-        if isinstance(val, RelativeCoordinate):
-            return val
-        elif all(axis in val for axis in CartesianCoordinateSystem.AXES):
-            return CartVector(**val)
-        elif all(axis in val for axis in JointCoordinateSystem.AXES):
-            return JointVector(**val)
-    else:
-        if isinstance(val, AbsoluteCoordinate):
-            return val
-        elif all(axis in val for axis in CartesianCoordinateSystem.AXES):
-            return CartCoord(**val)
-        elif all(axis in val for axis in JointCoordinateSystem.AXES):
-            return JointCoord(**val)
-    raise ValueError("It must have keys ('x','y','z','r') or ('j1','j2','j3','j4').")
+from abc import (ABCMeta, abstractmethod)
+from collections import abc
+from typing import (Union, Optional,
+                    Mapping, Iterator, SupportsFloat)
 
 # Abstract Classes
 class MetaCoordinate(ABCMeta):
+    """ 座標型が軸を持つことを保証するためのメタクラスです。 """
     def __new__(mcls, *args, **kwargs):
         cls = super().__new__(mcls, *args, **kwargs)
         if not hasattr(cls, "AXES"):
@@ -31,19 +16,21 @@ class MetaCoordinate(ABCMeta):
             setattr(cls, axis, property(lambda self: self[axis]))
         return cls
 
-class Coordinate(Mapping, metaclass=MetaCoordinate):
+class Coordinate(abc.Mapping, metaclass=MetaCoordinate):
+    """ 座標を表す抽象クラスです。 """
     AXES = tuple()
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: str) -> float:
+        """ 軸を表す文字列を代入すると、その軸のパラメータを返します。 """
         if key in self.AXES:
             return getattr(self, "_"+key)
         else:
             raise KeyError()
     
-    def __iter__(self):
+    def __iter__(self) -> Iterator[str]:
         return iter(self.AXES)
     
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.AXES)
     
     @abstractmethod
@@ -58,57 +45,65 @@ class Coordinate(Mapping, metaclass=MetaCoordinate):
             setattr(self, "_" + axis, arg)
     
     @abstractmethod
-    def is_relative(self):
+    def is_relative(self) -> bool:
+        """ Dobot の原点を中心とした絶対座標かそうでないかを bool で返します。 """
         pass
     
     #@abstractmethod
-    def __add__(self, other):
+    def __add__(self, other:Mapping[str, SupportsFloat]) -> 'Coordinate':
         pass
     #@abstractmethod
-    def __sub__(self, other):
+    def __sub__(self, other:Mapping[str, SupportsFloat]) -> 'Coordinate':
         pass
     #@abstractmethod
-    def __mul__(self, val):
+    def __mul__(self, val:SupportsFloat) -> 'Coordinate':
         pass
     #@abstractmethod
-    def __div__(self, val):
+    def __div__(self, val:SupportsFloat) -> 'Coordinate':
         pass
 
 class AbsoluteCoordinate(Coordinate):
+    """ Dobot の原点を中心とした絶対座標であることを表すクラスです。 """
     #@abstractmethod
-    def is_valid(self):
-        """ 可動域の中かどうかを返す関数 """
+    def is_valid(self) -> bool:
+        """ 自分自身が Dobot の可動域の中かどうかを返す関数。 """
         pass
     
-    def is_relative(self):
+    def is_relative(self) -> bool:
         return False
 
-    def infiltrate(self, target, labels={}):
-        """
-        API用の構造体に自分の値を挿入
+    def infiltrate(self, target:ctypes.Structure,
+                         labels:Mapping[str,Optional[str]] = {}) -> ctypes.Structure:
+        """ 内部でAPIを送信するためのメソッドです。構造体を引数に取り、自身の値をその構造体に代入します。
 
-        Parameters
-        ----------
-        target: ctypes.Structure
-            挿入するための構造体
-        labels: dict
-            軸の名称をキーとして、構造体のラベルを代入
+        labels は Coordinate オブジェクトでの軸の名称から構造体に代入したい要素名への dict です。
+        None を指定するとその軸の値が無視されます。
         """
         for axis in self.AXES:
             if axis in labels:
-                setattr(target, labels[axis], self[axis])
-            else:
-                setattr(target, axis, self[axis])
+                setattr(target, labels[axis], float(self[axis]))
+            elif self[axis] is not None:
+                    setattr(target, axis, float(self[axis]))
         return target
 
 class RelativeCoordinate(Coordinate):
+    """ 相対座標であることを表すクラスです。 """
     def is_relative(self):
         return True
 
 class CartesianCoordinateSystem(Coordinate):
+    """ デカルト座標系を表すクラスです。先端部分の回転角度も持ちます。
+    
+    x は Dobot の正面（土台の各種配線のある部分の反対側）を正とします。
+    y はこの正面を向いて左側を正とします。z は上向きを正とします。
+    x, y, z の単位はそれぞれ mm です。
+    r は上から見て反時計回りを正とするエンドエフェクタの角度です。度数法で表します。
+    """
     AXES = ("x", "y", "z", "r")
 
-    def __init__(self, x, y, z, r=0):
+    def __init__(self, x, y, z, r=0.0) -> 'CartesianCoordinateSystem':
+        if type(self) is CartesianCoordinateSystem:
+            raise TypeError("You can't create an instance of `CoordinateSystem` class.")
         super().__init__(x, y, z, r)
 
     def scalar_product(self, other):
@@ -116,56 +111,74 @@ class CartesianCoordinateSystem(Coordinate):
 
 
 class JointCoordinateSystem(Coordinate):
+    """ 各関節の角度で示される座標系であることを表すクラスです。 """
     AXES = ("j1", "j2", "j3", "j4")
 
-    def __init__(self, j1, j2, j3, j4):
+    def __init__(self, j1, j2, j3, j4) -> 'JointCoordinateSystem':
+        if type(self) is JointCoordinateSystem:
+            raise TypeError("You can't create an instance of `CoordinateSystem` class.")
         super().__init__(j1, j2, j3, j4)
 
 
 # Partical Classes
-
 class CartCoord(CartesianCoordinateSystem, AbsoluteCoordinate):
-    def __init__(self, x, y, z, r, check=False):
-        """
-        Parameters
-        ----------
-        x: float
-            肩の関節の中心を原点とし、前方を正とする座標軸のエンドエフェクタのモーター付け根部分の位置。単位はmm。
-        y: float
-            アームから見て左側を正とする座標軸。
-        z: float
-            上方を正とする座標軸。
-        r: float
-            上から見て反時計回りを正とするエンドエフェクタの角度。単位は度数法。
-        check: bool
-            代入された値が可動域内かどうかを試験する（未実装）
-        """
+    """ アームの軸の中心を原点とした、3次元デカルト座標と先端部分の回転角度です。
+
+    原点は Dobot の左右に突き出したモーター部分の中点、
+    座標の指し示す場所は純正エンドエフェクタを付けた場合のエンドエフェクタの取り付け部分になります。
+    各軸の詳細は CartesianCoordinateSystem の説明参照のこと。
+    """
+    def __init__(self, x:SupportsFloat, y:SupportsFloat,
+                       z:SupportsFloat, r:SupportsFloat=0.0, check:bool=False):
+        """ check を True にすると指定した座標が可動域に収まっているかどうかが判定されます。（未実装） """
         super().__init__(x, y, z, r)
         if check and not self.is_valid():
             raise ValueError("This position is invalid")
 
 class CartVector(CartesianCoordinateSystem, RelativeCoordinate):
+    """ 3次元デカルト座標で相対座標を示すものです。
+    
+    各軸の詳細は CartesianCoordinateSystem の説明参照のこと。
+    """
     pass
 
 class JointCoord(JointCoordinateSystem, AbsoluteCoordinate):
-    def __init__(self, j1, j2, j3, j4, check=False):
-        """
-        Parameters
-        ----------
-        j1: float
-            肩の関節の中心を原点とし、前方を正とする座標軸のエンドエフェクタのモーター付け根部分の位置。単位はmm。
-        j2: float
-            アームから見て左側を正とする座標軸。
-        j3: float
-            上方を正とする座標軸。
-        j4: float
-            上から見て反時計回りを正とするエンドエフェクタの角度。単位は度数法。
-        check: bool
-            代入された値が可動域内かどうかを試験する（未実装）
-        """
+    """ 各関節の角度で、アームの姿勢を表したものです。
+
+    各関節の詳細は JointCoordinateSystem の説明参照のこと。
+    """
+    def __init__(self, j1:SupportsFloat, j2:SupportsFloat,
+                       j3:SupportsFloat, j4:SupportsFloat, check:bool=False):
+        """ check を True にすると指定した座標が可動域に収まっているかどうかが判定されます。（未実装） """
         super().__init__(j1, j2, j3, j4)
         if check and not self.is_valid():
             raise ValueError("This position is invalid")
 
 class JointVector(JointCoordinateSystem, RelativeCoordinate):
+    """ 各関節の相対的な角度を示すものです。
+    
+    各関節の詳細は CartesianCoordinateSystem の説明参照のこと。
+    """
     pass
+
+
+# methods
+def convert_coord(point: Mapping[str, SupportsFloat], relative: bool) -> Coordinate:
+    """ point を適切な座標クラスに変換するための関数です。 """
+    if not isinstance(point, Mapping[str, SupportsFloat]):
+        raise TypeError("The point must be an object shows any coordinate.")
+    if relative:
+        if isinstance(point, RelativeCoordinate):
+            return point
+        elif all(axis in point for axis in CartesianCoordinateSystem.AXES):
+            return CartVector(**point)
+        elif all(axis in point for axis in JointCoordinateSystem.AXES):
+            return JointVector(**point)
+    else:
+        if isinstance(point, AbsoluteCoordinate):
+            return point
+        elif all(axis in point for axis in CartesianCoordinateSystem.AXES):
+            return CartCoord(**point)
+        elif all(axis in point for axis in JointCoordinateSystem.AXES):
+            return JointCoord(**point)
+    raise ValueError("It must have keys ('x','y','z','r') or ('j1','j2','j3','j4').")
